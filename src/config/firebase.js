160f3fde614632adf5de120f59f0060956d7cbcc
Preben Andersen <preb1anders1@gmail.com>
2023-10-17 23:12:13 +0200

    changed to firebase var src to env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,18 +8,18 @@ import {getAuth} from "firebase/auth";
 
 // Your web app's Firebase configuration
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCIJIB1KZvIH4UT4ZmUN7yk1hmHwQ-ETWw",
-  authDomain: "chatgptinstructions.firebaseapp.com",
-  projectId: "chatgptinstructions",
-  storageBucket: "chatgptinstructions.appspot.com",
-  messagingSenderId: "450895086920",
-  appId: "1:450895086920:web:2204b1d442fda9f7e0df7d",
-  measurementId: "G-FW8YSDS7SH"
-};
 
+// Environment variables stored in '.env'
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
 
 export const auth = getAuth();
