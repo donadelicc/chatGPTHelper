acf125b805fb41cdd748e0bf76a01aea30add654
Preben Andersen <preb1anders1@gmail.com>
2023-10-28 01:07:25 +0200

    major db upd

--- src/pages/CreateInstructionPage.jsx
+++ src/pages/CreateInstructionPage.jsx
@@ -1,19 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import styles from '../styles/hei.module.css'
-// sjsjs
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import {getAuth} from "firebase/auth";
+
+
+const gloabalDb = 'globalInstruction'
+const personalDb = 'user'
+
 
 const CreateInstructionPage = () => {
   const [instructionType, setInstructionType] = useState('');
+  const [instructionCategory, setInstructionCategory] = useState('');
   const [instructionContent, setInstructionContent] = useState('');
   const [isValid, setIsValid] = useState(false);
   const [nameMissingError, setNameMissingError] = useState('');
+  const [categoryMissingError, setCategoryMissingError] = useState('');
   const [emptyInstructionError, setEmptyInstructionError] = useState('');
+
+  // Må disse initalliseres på nytt? Kan ikke disse bare importeres fra firebase.js?
+  const db = getFirestore();
+  const auth = getAuth();
 
   const handleNameChange = (e) => {
     setInstructionType(e.target.value);
-    setNameMissingError(''); 
+    setNameMissingError('');
   };
+
+  const handleCategoryChange = (e) => {
+    setInstructionCategory(e.target.value);
+    setCategoryMissingError('');
+  };
+
 
   const handleInstructionsChange = (e) => {
     setInstructionContent(e.target.value);
@@ -22,45 +40,75 @@ const CreateInstructionPage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  
+
     setNameMissingError('');
+    setCategoryMissingError('');
     setEmptyInstructionError('');
-  
+
     if (instructionType === '') {
       setNameMissingError('You need to provide a name for your instruction');
     }
+
+    if (instructionCategory === '') {
+      setCategoryMissingError('You need to provide a category for your instruction');
+    }
+
     if (instructionContent === '') {
       setEmptyInstructionError('You need to provide some instructions');
     }
-  
-    if (instructionType !== '' && instructionContent !== '') {
+
+    if (instructionType !== '' && instructionContent !== '' && instructionCategory !== '') {
       const sentenceArray = instructionContent.split("\n");
 
       const instruction = {
         header: instructionType,
-        instruction: sentenceArray,
+        category: instructionCategory,
+        instructionArray: sentenceArray,
       };
-
       console.table(instruction)
-      
+
+      // Referanse til brukerens dokument
+      const userRef = db.collection(personalDb).doc(auth.currentUser.uid);
+
+      // Legg til eller oppdater brukerinfo
+      userRef.set({
+        email: auth.currentUser.email,
+      }, { merge: true })  // { merge: true } vil sørge for at eksisterende data ikke overskrives
+        .then(() => {
+          console.log('User added to database');
+        })
+        .catch((error) => {
+          console.error('Error adding user to database: ', error);
+        });
+
+      // Legg til instruksjon i brukerens sub-kolleksjon
+      userRef.collection('instructions').add(instruction)
+        .then((docRef) => {
+          console.log('Instruction added with ID: ', docRef.id);
+        })
+        .catch((error) => {
+          console.error('Error adding instruction: ', error);
+        });
+
+
     }
   };
-  
+
 
   useEffect(() => {
-    if (instructionType !== '' && instructionContent !== '') {
+    if (instructionType !== '' && instructionCategory && instructionContent !== '') {
       setIsValid(true);
     } else {
       setIsValid(false);
     }
-  }, [instructionContent, instructionType]);
+  }, [instructionContent, instructionCategory, instructionType]);
 
   return (
     <>
       <Head>
         <title>Create Instruction</title>
       </Head>
-    {/* <NavBar /> */}
+      {/* <NavBar /> */}
       <div className={styles.mainContainer}>
         <div className={styles.main__content}>
           <div className={styles.headerContainer}>
@@ -98,7 +146,14 @@ const CreateInstructionPage = () => {
                 <label className={styles.formLabel}>instructions</label>
                 <p className={styles.errorMessage} >{emptyInstructionError}</p>
               </div>
-              
+
+              <input
+                type="text"
+                className={styles.categoryInput}
+                onChange={handleCategoryChange}
+                value={instructionCategory}
+              />
+
               <textarea
                 className={styles.textarea}
                 onChange={handleInstructionsChange}
@@ -109,9 +164,8 @@ const CreateInstructionPage = () => {
             <div className={styles.field}>
               <button
                 type='submit'
-                className={`${styles.submitButton} ${
-                  !isValid ? styles.disabled : ''
-                }`}
+                className={`${styles.submitButton} ${!isValid ? styles.disabled : ''
+                  }`}
               >
                 Save instruction
               </button>
