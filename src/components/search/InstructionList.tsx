ed31ea201322da62a9c47a6e4fae7e3d4f353bc2
Jonas Andersen <Jonas@anders1.com>
2023-10-28 02:05:02 +0200

    update KEY

--- src/components/search/InstructionList.tsx
+++ src/components/search/InstructionList.tsx
@@ -5,13 +5,15 @@ import InstructionItem from './InstructionItem';
 
 
 const InstructionList = () => {
+  const randInd  = Math.random() * 10000
+
 
   const { instructionSets: instructions, setInstructionSets: setInstructions } = useInstructions();
   
   return (
     <div style={{display:"flex", flexDirection:"column", gap:10, height:"100%", overflow:"scroll"}}>
-     {instructions.map((instruction, index) => (
-      <InstructionItem key={index} index={index} header={instruction.header} instruction={instruction.instruction} />
+     {instructions.map((instruction, randInd) => (
+      <InstructionItem key={randInd} index={randInd} header={instruction.header} instruction={instruction.instruction} />
      ))}
     </div>
   )
